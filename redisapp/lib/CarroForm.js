1b1cf6ff13f71ea08a0f628349355e3462d45f5b
BrunoCostaPrado <brunocostaprado@gmail.com>
2022-01-14 00:47:15 -0300

    .

---
+++ redisapp/lib/CarroForm.js
@@ -0,0 +1,28 @@
+export default function CarForm() {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const formdata = Object.fromEntries(form.entries());
+    console.log(formdata);
+    
+    const res = await fetch('/api/car', {
+      body: JSON.stringify(formdata),
+      headers: {
+        "Content=Type": "application/json",
+      },
+      method: "POST",
+    });
+    const result=await res.JSON();
+    console.log(result)
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="make" type="text" />
+      <input name="model" type="text" />
+      <input name="image" type="text" />
+      <textarea name="description" type="text" />
+      <button type="submit">Criar Carro</button>
+    </form>
+  );
+}
